0bcffcd801fb118d5e05edfd4361b29be97d1fcd
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-05-13 20:50:35 +0200

    Update darkwin.cmd

--- darkwin.cmd
+++ darkwin.cmd
@@ -33,14 +33,18 @@ echo [1]: Verificar si hay problemas de energia en el sistema
 echo [2]: Limpiar mi equipo
 echo [3]: Ajustar los parametros de ejecucion de VBScript
 echo [4]: Desocultar los archivos de cierta unidad
+echo [5]: Actualizar todos los programas que necesiten actualizacion
+echo [6]: Actualizar todos los drivers que necesiten actualizacion
 echo ------------------------------------------------------------------------------------------------------
 set /p choice= [+] INGRESE LA OPCION QUE HA ELEGIDO: 
 if /i %choice%==0 goto exit
 if /i %choice%==1 goto verify
 if /i %choice%==2 goto clean
 if /i %choice%==3 goto vbs
-if /i %choice%==3 goto des
-if not /i exist %choice%: goto error
+if /i %choice%==4 goto des
+if /i %choice%==5 goto update
+if /i %choice%==6 goto drivers
+if /i %choice%==3 goto if not /i exist %choice%: goto error
 
 :verify
 color 0c
@@ -52,7 +56,7 @@ goto menu
 :clean
 color 0c
 title The DarkWin tamagochi for Windows
-cleanmgr & sfc /scannow & dism /online /cleanup-image /restorehealth & chkdsk C: /f /r & dism.exe /online /Cleanup-Image /StartComponentCleanup & defrag C:
+cleanmgr & sfc /scannow & dism /online /cleanup-image /restorehealth & chkdsk C: /f /r & dism.exe /online /Cleanup-Image /StartComponentCleanup & defrag C: /r & netsh int ip reset
 pause
 goto menu
 
@@ -72,6 +76,20 @@ echo ---------------------------------------------------------------------------
 set /p choice= [+] INGRESE LA LETRA DE LA UNIDAD A DESOCULTAR: 
 echo ------------------------------------------------------------------------------------------------------
 cd %choice%:\ & attrib -r -a -h -s %choice%:\*.* /S /D
+pause
+goto menu
+
+:update
+color 0c
+title The DarkWin tamagochi for Windows
+winget upgrade --all
+pause
+goto menu
+
+:drivers
+color 0c
+title The DarkWin tamagochi for Windows
+wuauclt /detectnow
 pause
 goto menu
 
